5548c27ce424daf3f9d8deae90ba220d45a04415
Kyle <ksmontville@gmail.com>
2024-06-04 11:58:25 -0700

    add logging

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def data_validator(request):
     if request.method == 'POST':
         request_json = request.get_json(silent=True)
         if request_json:
-            lab_id = request_json.get('lab_id', None)
+            lab_ids = request_json.get('lab_ids', [])
             is_from_guest = request_json.get('is_from_guest', False)
             if is_from_guest:
                 os.environ['guest_mode'] = "True"
@@ -39,56 +39,67 @@ def data_validator(request):
             prefix_name = request_json.get('prefix_name', None)
             start_date = request_json.get('start_date', None) # '04/01/2024'
             end_date = request_json.get('end_date', None)  # '04/01/2024'
-            if params_check(lab_id, is_from_firestore, is_save_to_storage, is_upload_to_redivis, is_release_on_redivis,
-                            prefix_name):
-                storage = StorageServices(lab_id=lab_id, is_from_firestore=is_from_firestore)
-                ec = EntityController(is_from_firestore=is_from_firestore)
-                if prefix_name:  # if prefix_name specified, go to uploading_to_redivis process.
-                    storage.storage_prefix = prefix_name
-                else:  # if no prefix_name specified, start validation process.
-                    if is_from_firestore:
-                        ec.set_values_from_firestore(lab_id=lab_id, start_date=start_date, end_date=end_date)
-                    elif lab_id != 'all':
-                        ec.set_values_for_consolidate()
-                    else:
-                        ec.set_values_from_redivis(lab_id=lab_id, is_consolidate=False)
-                    print(f"validation_log_list: {ec.validation_log}")
 
-                    # GCP storage service
-                    if is_save_to_storage:
-                        storage.process(valid_data=ec.get_valid_data(), invalid_data=ec.get_invalid_data())
-                        print(f"upload_to_GCP_log_list: {storage.upload_to_GCP_log}")
-                    else:
-                        output = {'title': f'Function executed successfully!',
-                                  'valid_users_count': len(ec.valid_users),
-                                  'valid_runs_count': len(ec.valid_runs),
-                                  'valid_trials_count': len(ec.valid_trials),
-                                  'logs': ec.validation_log,
-                                  'invalid_results': ec.get_invalid_data()}
+            for lab_id in lab_ids:
+                job = 1
+                print(f'Syncing data from Firestore to Redivis for lab_id: {lab_id}; {job} of {len(lab_ids)}.')
+                if params_check(lab_id, is_from_firestore, is_save_to_storage,
+                                is_upload_to_redivis, is_release_on_redivis,
+                                prefix_name):
+                    storage = StorageServices(lab_id=lab_id, is_from_firestore=is_from_firestore)
+                    ec = EntityController(is_from_firestore=is_from_firestore)
+                    if prefix_name:  # if prefix_name specified, go to uploading_to_redivis process.
+                        storage.storage_prefix = prefix_name
+                    else:  # if no prefix_name specified, start validation process.
+                        if is_from_firestore:
+                            print(f'Getting data from Firestore for lab_id: {lab_id}.')
+                            ec.set_values_from_firestore(lab_id=lab_id, start_date=start_date, end_date=end_date)
+                        elif lab_id != 'all':
+                            ec.set_values_for_consolidate()
+                        else:
+                            ec.set_values_from_redivis(lab_id=lab_id, is_consolidate=False)
+                        print(f"validation_log_list: {ec.validation_log}")
+
+                        # GCP storage service
+                        if is_save_to_storage:
+                            print(f"Saving data to GCP storage for lab_id: {lab_id}.")
+                            storage.process(valid_data=ec.get_valid_data(), invalid_data=ec.get_invalid_data())
+                            print(f"upload_to_GCP_log_list: {storage.upload_to_GCP_log}")
+                        else:
+                            output = {'title': f'Function executed successfully!',
+                                      'valid_users_count': len(ec.valid_users),
+                                      'valid_runs_count': len(ec.valid_runs),
+                                      'valid_trials_count': len(ec.valid_trials),
+                                      'logs': ec.validation_log,
+                                      'invalid_results': ec.get_invalid_data()}
+                            job += 1
+                            return output, 200
+                    # redivis service
+                    if is_upload_to_redivis:
+                        print(f"Uploading data to Redivis for lab_id: {lab_id}.")
+                        rs = RedivisServices(is_from_firestore=is_from_firestore)
+                        rs.set_dataset(lab_id=lab_id)
+                        rs.create_dateset_version()
+                        # rs.save_to_redivis_table(file_name="lab_guests_firestore_2024-03-11-19-23-32/trials.json")
+                        file_names = storage.list_blobs_with_prefix()
+                        for file_name in file_names:
+                            rs.save_to_redivis_table(file_name=file_name)
+                        if not ec.get_invalid_data():
+                            rs.delete_table(table_name='validation_results')
+                        if is_release_on_redivis:
+                            rs.release_dataset()
+                        print(f"upload_to_redivis_log_list: {rs.upload_to_redivis_log}")
+                        output = {'title': f'Function executed successfully! Current DS has {rs.count_tables()} tables.',
+                                  'logs': rs.upload_to_redivis_log}
+                        job += 1
                         return output, 200
-                # redivis service
-                if is_upload_to_redivis:
-                    rs = RedivisServices(is_from_firestore=is_from_firestore)
-                    rs.set_dataset(lab_id=lab_id)
-                    rs.create_dateset_version()
-                    # rs.save_to_redivis_table(file_name="lab_guests_firestore_2024-03-11-19-23-32/trials.json")
-                    file_names = storage.list_blobs_with_prefix()
-                    for file_name in file_names:
-                        rs.save_to_redivis_table(file_name=file_name)
-                    if not ec.get_invalid_data():
-                        rs.delete_table(table_name='validation_results')
-                    if is_release_on_redivis:
-                        rs.release_dataset()
-                    print(f"upload_to_redivis_log_list: {rs.upload_to_redivis_log}")
-                    output = {'title': f'Function executed successfully! Current DS has {rs.count_tables()} tables.',
-                              'logs': rs.upload_to_redivis_log}
-                    return output, 200
-                elif is_save_to_storage and not prefix_name:
-                    output = {'title': f'Function executed successfully! Data uploaded to GCP storage only.',
-                              'logs': storage.upload_to_GCP_log}
-                    return output, 200
-                else:
-                    return 'Error in parameters setup in the request body', 400
+                    elif is_save_to_storage and not prefix_name:
+                        output = {'title': f'Function executed successfully! Data uploaded to GCP storage only.',
+                                  'logs': storage.upload_to_GCP_log}
+                        job += 1
+                        return output, 200
+                    else:
+                        return 'Error in parameters setup in the request body', 400
             else:
                 return 'Error in parameters setup in the request body', 400
         else:
